b99a2240610dad59496acae97854e87ff6c2a33b
Anderson Wesley <andersonwbezerra@hotmail.com>
2020-10-09 13:31:17 -0400

    Nivel 03: Gobaber Web - Páginade Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import SignIn from '../src/pages/SignIn/index';
 import GlobalStyle from '../src/styles/global';
 
 const App: React.FC = ()=>(
   <>
-    <h1>Hello World!!</h1>
+    <SignIn/>
     <GlobalStyle/>
   </>
 );
