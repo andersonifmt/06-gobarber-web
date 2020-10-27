cc51a629b48013e40dd1afc6ad78c2ce09505aea
Anderson Wesley <andersonwbezerra@hotmail.com>
2020-10-27 08:16:58 -0400

    Nivel 03: Gobaber Web - Validação de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-//import SignIn from '../src/pages/SignIn/index';
-import SignUp from '../src/pages/SignUp/index';
+import SignIn from '../src/pages/SignIn/index';
+//import SignUp from '../src/pages/SignUp/index';
 import GlobalStyle from '../src/styles/global';
 
 const App: React.FC = ()=>(
   <>
-    <SignUp/>
+    <SignIn/>
     <GlobalStyle/>
   </>
 );
