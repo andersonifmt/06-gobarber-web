c82b31eb67f95b3d700eac975570d5e10f2fdd39
Anderson Wesley <andersonwbezerra@hotmail.com>
2020-12-26 07:49:52 -0400

    Nivel 03: Gobaber Web - Finalizando App

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,7 +3,7 @@ import {FiLogIn, FiMail, FiLock} from 'react-icons/fi';
 import {FormHandles} from '@unform/core';
 import {Form} from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -28,6 +28,8 @@ const SignIn: React.FC = () =>{
   const { user, signIn } = useAuth();
   const { addToast } = useToast();
 
+  const history = useHistory();
+
   console.log(user);
 
   const handleSubmit=useCallback(async (data: SignInFormData) => {
@@ -47,6 +49,8 @@ const SignIn: React.FC = () =>{
         email: data.email,
         password: data.password,
       });
+
+      history.push('/dashboard');
     } catch (err) {
       if(err instanceof Yup.ValidationError){
         const erros = getValidationErros(err);
@@ -61,7 +65,7 @@ const SignIn: React.FC = () =>{
       }
       );
     }
-  },[signIn, addToast]);
+  },[signIn, addToast, history]);
 
   return (
     <Container>
